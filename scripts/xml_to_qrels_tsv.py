52c86ce7d49d21a71d73e92afe38cbc3a28ba639
Vít Novotný <witiko@gmail.com>
2020-06-03 18:23:46 +0200

    Add topics-formula_ids-qrels.V1.1.tsv

--- scripts/xml_to_qrels_tsv.py
+++ scripts/xml_to_qrels_tsv.py
@@ -1,28 +1,43 @@
 # -*- coding:utf-8 -*-
 
 import csv
+import sys
 
 from arqmathcode.post_reader_record import DataReaderRecord
+from arqmathcode.topic_file_reader import TopicReader
 from tqdm import tqdm
 
-from .configuration import CSV_PARAMETERS, ARQMATH_COLLECTION_INPUT_DATA_DIRNAME, ARQMATH_COLLECTION_QRELS_FILENAME
+from .configuration import CSV_PARAMETERS, ARQMATH_COLLECTION_INPUT_DATA_DIRNAME, ARQMATH_COLLECTION_QRELS_FILENAME, ARQMATH_TASK2_QRELS_FILENAME, ARQMATH_TASK2_TEST_INPUT_POSTS_FILENAME
+
+
+assert sys.argv[1] in ('collection', 'task2-topics-formula_ids')
+if sys.argv[1] == 'collection':
+    ARQMATH_QRELS_FILENAME = ARQMATH_COLLECTION_QRELS_FILENAME
+elif sys.argv[1] == 'task2-topics-formula_ids':
+    ARQMATH_QRELS_FILENAME = ARQMATH_TASK2_QRELS_FILENAME
 
 
 if __name__ == '__main__':
-    xml_reader = DataReaderRecord(ARQMATH_COLLECTION_INPUT_DATA_DIRNAME)
-    with open(ARQMATH_COLLECTION_QRELS_FILENAME, 'wt') as f:
+    if sys.argv[1] == 'collection':
+        xml_reader = DataReaderRecord(ARQMATH_COLLECTION_INPUT_DATA_DIRNAME)
+        questions = xml_reader.post_parser.map_questions.values()
+        questions = sorted(questions, key=lambda question: question.post_id)
+    elif sys.argv[1] == 'task2-topics-formula_ids':
+        xml_reader = TopicReader(ARQMATH_TASK2_TEST_INPUT_POSTS_FILENAME)
+        questions = xml_reader._TopicReader__map_topics.values()
+        questions = sorted(questions, key=lambda question: question.topic_id)
+    with open(ARQMATH_QRELS_FILENAME, 'wt') as f:
         csv_writer = csv.writer(f, **CSV_PARAMETERS)
-        questions = tqdm(
-            sorted(
-                xml_reader.post_parser.map_questions.values(),
-                key=lambda question: question.post_id
-            ),
-            desc='Converting',
-        )
+        questions = tqdm(questions, desc='Converting')
         for question in questions:
-            for answer in (question.answers or []):
-                positive_votes = sum(1 for vote in (answer.votes or []) if vote.vote_type_id == 2)
-                negative_votes = sum(1 for vote in (answer.votes or []) if vote.vote_type_id == 3)
-                judgement = max(0, positive_votes - negative_votes)
-                row = (question.post_id, 'xxx', answer.post_id, judgement)
+            if sys.argv[1] == 'collection':
+                for answer in (question.answers or []):
+                    positive_votes = sum(1 for vote in (answer.votes or []) if vote.vote_type_id == 2)
+                    negative_votes = sum(1 for vote in (answer.votes or []) if vote.vote_type_id == 3)
+                    judgement = max(0, positive_votes - negative_votes)
+                    row = (question.post_id, 'xxx', answer.post_id, judgement)
+                    csv_writer.writerow(row)
+            elif sys.argv[1] == 'task2-topics-formula_ids':
+                answer = question.title
+                row = (answer, 'xxx', question.topic_id, 0)
                 csv_writer.writerow(row)
